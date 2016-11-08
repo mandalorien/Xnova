ee393a2bd266201603f8ecade37d2f63ea28d340
mandalorien wootook <mandalorien.wootook@gmail.com>
2016-11-08 16:12:30 +0100

    modification de la config du routing , utilisation des annotations .

--- src/CoreBundle/Controller/CoreController.php
+++ src/CoreBundle/Controller/CoreController.php
@@ -2,12 +2,19 @@
 
 namespace CoreBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class CoreController extends Controller
 {
+    /**
+     * @Route("/", name="login")
+     * @Template("CoreBundle:Core:index.html.twig")
+     */
     public function indexAction()
     {
-        return $this->render('CoreBundle:Core:index.html.twig');
+
     }
 }
